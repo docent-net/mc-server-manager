e05bf72ea59dad67fd62e6278fc70274f52fff49
Maciej Lasyk <maciej@lasyk.info>
2020-04-28 23:17:12 +0200

    server_manager: testServerExistance #6

--- backend/tests/test_server_manager.py
+++ backend/tests/test_server_manager.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from unittest import TestCase, main, mock
+from pathlib import Path
 from mc_srv_manager.server_manager import server_manager
 
 
@@ -11,4 +12,16 @@ class testConfig(TestCase):
     def tearDown(self) -> None:
         pass
 
-    #TODO: tests
+    def testServerExistance(self) -> None:
+        srv_manager = server_manager()
+        with mock.patch.object(Path, 'exists') as mock_exists:
+            mock_exists.return_value = False
+            self.assertEqual(
+                            srv_manager.check_if_server_exists('bous_server'),
+                            False
+                            )
+            mock_exists.return_value = True
+            self.assertEqual(
+                            srv_manager.check_if_server_exists('bous_server'),
+                            True
+                            )
