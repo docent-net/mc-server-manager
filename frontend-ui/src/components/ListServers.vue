90bf79ee30033dec777528b2ef744893a35b2735
Maciej Lasyk <maciej@lasyk.info>
2020-05-02 18:19:29 +0200

    feat/UI poc WIP #4

---
+++ frontend-ui/src/components/ListServers.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="servers-list-component">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'ListServersComponent',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:8000/get_servers';
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
+  },
+};
+</script>
---
+++ frontend-ui/src/views/ListServers.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="servers-page">
+    <ListServers msg="MC server manager"/>
+    view test
+  </div>
+</template>
+
+<script>
+import ListServers from '@/components/ListServers.vue';
+
+export default {
+  name: 'ListServersView',
+  components: {
+    ListServers,
+  },
+};
+</script>
