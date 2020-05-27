f8999ab32bba6519d0d1335c6f7364e62c27ed78
Maciej Lasyk <maciej@lasyk.info>
2020-05-27 13:37:32 +0200

    testing tests

--- frontend-ui/tests/unit/example.spec.js
+++ frontend-ui/tests/unit/example.spec.js
@@ -3,6 +3,7 @@ import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 // 2
 // 1
+// 3
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
